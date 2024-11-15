5a5547e80d67558940a0d47c282a7a045a973819
berghetti <fabricio@node0.fabricio.demeter-pg0.clemson.cloudlab.us>
2024-11-15 10:59:17 -0500

    fix dpdk compilation

--- setup.sh
+++ setup.sh
@@ -8,12 +8,17 @@ rmmod dune
 sudo sh -c 'for i in /sys/devices/system/node/node*/hugepages/hugepages-2048kB/nr_hugepages; do echo 8192 > $i; done'
 
 # Unbind NICs
- ./deps/dpdk/tools/dpdk_nic_bind.py -u 03:00.1
+./deps/dpdk/tools/dpdk_nic_bind.py -u 18:00.1
 
 # Build required kernel modules.
 make -sj64 -C deps/dune
 make -sj64 -C deps/pcidma
 make -sj64 -C deps/dpdk config T=x86_64-native-linuxapp-gcc
+cd deps/dpdk
+    git apply ../dpdk.mk.patch
+    git apply ../dpdk_i40e.patch
+    git apply ../dpdk.vars.patch
+cd ../../
 make -sj64 -C deps/dpdk
 
 # Insert kernel modules
