07a377063a2bdaced765b8ce07ca1e9f1e195455
berghetti <mayco.s.berghetti@gmail.com>
2024-12-15 11:23:05 -0500

    use afp payload format to get request type!

--- inc/ix/dispatch.h
+++ inc/ix/dispatch.h
@@ -292,81 +292,80 @@ static inline int smart_tskq_dequeue(struct task_queue * tq, void ** rnbl_ptr,
 static inline struct request * rq_update(struct request_queue * rq, struct mbuf * pkt)
 {
 	// Quickly parse packet without doing checks
-        struct eth_hdr * ethhdr = mbuf_mtod(pkt, struct eth_hdr *);
-        struct ip_hdr *  iphdr = mbuf_nextd(ethhdr, struct ip_hdr *);
-        int hdrlen = iphdr->header_len * sizeof(uint32_t);
+  struct eth_hdr * ethhdr = mbuf_mtod(pkt, struct eth_hdr *);
+  struct ip_hdr *  iphdr = mbuf_nextd(ethhdr, struct ip_hdr *);
+  int hdrlen = iphdr->header_len * sizeof(uint32_t);
 	struct udp_hdr * udphdr = mbuf_nextd_off(iphdr, struct udp_hdr *,
                                                  hdrlen);
 	// Get data and udp header
 	void * data = mbuf_nextd(udphdr, void *);
-	struct message * msg = (struct message *) data;
-	uint16_t type = msg->type;
-        uint16_t seq_num = msg->seq_num;
-        uint16_t client_id = msg->client_id;
-        uint32_t req_id = msg->req_id;
-        uint32_t pkts_length = msg->pkts_length / sizeof(struct message);
-	if (pkts_length == 1) {
-		struct request * req = mempool_alloc(&request_mempool);
-		req->type = type;
-		req->pkts_length = 1;
-		req->mbufs[0] = pkt;
-		return req;
-	}
+	//struct message * msg = (struct message *) data;
+	uint64_t *afp_payload  = data;
+	//uint16_t type = msg->type;
+  //uint16_t seq_num = msg->seq_num;
+  //uint16_t client_id = msg->client_id;
+  //uint32_t req_id = msg->req_id;
+  //uint32_t pkts_length = msg->pkts_length / sizeof(struct message);
+	struct request * req = mempool_alloc(&request_mempool);
+	req->type = afp_payload[3];
+	req->pkts_length = 1;
+	req->mbufs[0] = pkt;
+	return req;
 
-        if (!rq->head) {
-                struct request_cell * rc = mempool_alloc(&rq_mempool);
-                rc->pkts_remaining = pkts_length - 1;
-                rc->client_id = client_id;
-                rc->req_id = req_id;
-                rc->req = mempool_alloc(&request_mempool);
-                rc->req->mbufs[seq_num] = pkt;
-                rc->req->pkts_length = pkts_length;
-                rc->req->type = type;
-                rc->next = NULL;
-                rc->prev = NULL;
-                rq->head = rc;
-                return NULL;
-        }
-	struct request_cell * cur = rq->head;
-        while (cur != NULL) {
-                if (cur->client_id == client_id && cur->req_id == req_id) {
-                        cur->req->mbufs[seq_num] = pkt;
-                        cur->pkts_remaining--;
-			if (cur->pkts_remaining == 0) {
-				struct request * req = cur->req;
-				if (cur->prev == NULL) {
-					rq->head = cur->next;
-					if (rq->head != NULL)
-						rq->head->prev = NULL;
-				} else {
-					cur->prev->next = cur->next;
-					if (cur->next != NULL)
-						cur->next->prev = cur->prev;
-				}
-				mempool_free(&rq_mempool, cur);
-				return req;
-			}
-			return NULL;
-		}
-		cur = cur->next;
-        }
+  //      if (!rq->head) {
+  //              struct request_cell * rc = mempool_alloc(&rq_mempool);
+  //              rc->pkts_remaining = pkts_length - 1;
+  //              rc->client_id = client_id;
+  //              rc->req_id = req_id;
+  //              rc->req = mempool_alloc(&request_mempool);
+  //              rc->req->mbufs[seq_num] = pkt;
+  //              rc->req->pkts_length = pkts_length;
+  //              rc->req->type = type;
+  //              rc->next = NULL;
+  //              rc->prev = NULL;
+  //              rq->head = rc;
+  //              return NULL;
+  //      }
+	//struct request_cell * cur = rq->head;
+  //      while (cur != NULL) {
+  //              if (cur->client_id == client_id && cur->req_id == req_id) {
+  //                      cur->req->mbufs[seq_num] = pkt;
+  //                      cur->pkts_remaining--;
+	//		if (cur->pkts_remaining == 0) {
+	//			struct request * req = cur->req;
+	//			if (cur->prev == NULL) {
+	//				rq->head = cur->next;
+	//				if (rq->head != NULL)
+	//					rq->head->prev = NULL;
+	//			} else {
+	//				cur->prev->next = cur->next;
+	//				if (cur->next != NULL)
+	//					cur->next->prev = cur->prev;
+	//			}
+	//			mempool_free(&rq_mempool, cur);
+	//			return req;
+	//		}
+	//		return NULL;
+	//	}
+	//	cur = cur->next;
+  //      }
 
-        if (cur == NULL) {
-                struct request_cell * rc = mempool_alloc(&rq_mempool);
-                rc->pkts_remaining = pkts_length - 1;
-                rc->client_id = client_id;
-                rc->req_id = req_id;
-                rc->req = mempool_alloc(&request_mempool);
-                rc->req->mbufs[seq_num] = pkt;
-                rc->req->pkts_length = pkts_length;
-                rc->req->type = type;
-                rc->next = rq->head;
-		rc->next->prev = rc;
-		rc->prev = NULL;
-                rq->head = rc;
-                return NULL;
-        }
-        return NULL;
+  //      if (cur == NULL) {
+  //              struct request_cell * rc = mempool_alloc(&rq_mempool);
+  //              rc->pkts_remaining = pkts_length - 1;
+  //              rc->client_id = client_id;
+  //              rc->req_id = req_id;
+  //              rc->req = mempool_alloc(&request_mempool);
+  //              rc->req->mbufs[seq_num] = pkt;
+  //              rc->req->pkts_length = pkts_length;
+  //              rc->req->type = type;
+  //              rc->next = rq->head;
+	//	rc->next->prev = rc;
+	//	rc->prev = NULL;
+  //              rq->head = rc;
+  //              return NULL;
+  //      }
+  //      return NULL;
 }
 
 uint64_t timestamps[MAX_WORKERS];
